ce6d3702ffb57a56fefcc956f3a4d43fd140ad68
Vadym Stupakov <vadim.stupakov@gmail.com>
2018-12-04 20:41:03 +0200

    Removed useless packages

--- install.sh
+++ install.sh
@@ -8,25 +8,18 @@ fi
 cd $(dirname $(readlink -f $0))
 
 sudo apt install --yes \
+    python3-pip \
     vim \
     git \
     zsh \
     htop \
     meld
 
-if [[ "$(which pip3)" == "" ]]; then
-    sudo apt install python3-pip --yes
-fi
-
 
 pip3 install --upgrade  --user \
-    pip \
     pydf \
     speedtest-cli \
     ipython \
-    pipenv
-
-sudo apt purge python3-pip --yes
 
 if [[ ! -d $HOME/.oh-my-zsh ]]; then
     git clone https://github.com/robbyrussell/oh-my-zsh.git $HOME/.oh-my-zsh
