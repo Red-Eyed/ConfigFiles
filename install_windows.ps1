de659b4c3717d6ad4628e9bf435bf558c70d4fc9
Vadym Stupakov <vadim.stupakov@gmail.com>
2019-03-03 18:03:09 +0200

    Added installation script for windows

---
+++ install_windows.ps1
@@ -0,0 +1,17 @@
+
+#install choco
+If(-Not (Test-Path -Path "$env:ProgramData\Chocolatey")) {
+    Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+}
+
+choco install miniconda3 --params="'/AddToPath:1'" -y
+choco install -y
+choco install -y git
+choco install -y vim
+choco install -y meld
+choco install -y vscode
+choco install -y pycharm-community
+choco install -y qbittorrent
+choco install -y vlc
+choco install -y 7zip
+    
