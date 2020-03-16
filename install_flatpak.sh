ebda4824791d6023463572403f03b5019bd6bf3d
Vadym Stupakov <vadim.stupakov@gmail.com>
2020-03-16 18:49:05 +0200

    added update

--- install_flatpak.sh
+++ install_flatpak.sh
@@ -1,5 +1,5 @@
 sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-
+sudo flatpak update --noninteractive
 sudo flatpak install --noninteractive \
     com.github.tchx84.Flatseal \
     org.keepassxc.KeePassXC \
