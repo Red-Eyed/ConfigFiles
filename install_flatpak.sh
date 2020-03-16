8a6f9b73b4d16f776ce6a84ce546a2cc2e561c45
Vadym Stupakov <vadim.stupakov@gmail.com>
2020-03-16 18:42:26 +0200

    added sudo for flatpak

--- install_flatpak.sh
+++ install_flatpak.sh
@@ -1,6 +1,6 @@
 sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
-flatpak install --noninteractive \
+sudo flatpak install --noninteractive \
     com.github.tchx84.Flatseal \
     org.keepassxc.KeePassXC \
     com.slack.Slack \
