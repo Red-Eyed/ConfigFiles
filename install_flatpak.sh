7ca6ea5d262ededd0c32fe41ef510c38336a85b9
Vadym Stupakov <vadim.stupakov@gmail.com>
2020-03-27 16:26:31 +0200

    added maps and system flag

--- install_flatpak.sh
+++ install_flatpak.sh
@@ -1,19 +1,20 @@
-sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-sudo flatpak update --noninteractive
+flatpak --system remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+flatpak --system update --noninteractive
 
-sudo flatpak install --noninteractive \
+flatpak --system install --noninteractive \
     com.github.tchx84.Flatseal \
     org.keepassxc.KeePassXC \
     com.slack.Slack \
     com.skype.Client \
     org.telegram \
+    com.discordapp.Discord \
     com.github.wwmm.pulseeffects  \
     com.valvesoftware.Steam \
-    com.discordapp.Discord \
     org.gimp.GIMP \
     org.qbittorrent.qBittorrent \
     org.libreoffice.LibreOffice \
     org.gnome.seahorse.Application \
     org.filezillaproject.Filezilla \
     org.videolan.VLC \
-    org.gnome.Logs
+    org.gnome.Logs \
+    org.gnome.Maps
