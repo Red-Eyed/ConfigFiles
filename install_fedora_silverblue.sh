e1424878f122c1f83303187b606addd666db70c5
Vadym Stupakov <vadim.stupakov@gmail.com>
2020-04-06 23:30:55 +0300

    added upgrade after rpm fusion

--- install_fedora_silverblue.sh
+++ install_fedora_silverblue.sh
@@ -9,10 +9,11 @@ fi
 cd $(dirname $(readlink -f $0))
 
 # adding rpm fusion
-yes| sudo rpm-ostree install --idempotent --allow-inactive \
+yes | sudo rpm-ostree install --idempotent --allow-inactive \
     https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm \
     https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 
+yes | sudo rpm-ostree upgrade
 
 yes | sudo rpm-ostree install --idempotent --allow-inactive \
     file-roller \
