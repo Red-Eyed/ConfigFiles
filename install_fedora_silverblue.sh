70aeefda7200499775a2a88d6413e9461f945c7a
Vadym Stupakov <vadim.stupakov@gmail.com>
2020-04-06 22:29:52 +0300

    added base init for fedora silverblue

---
+++ install_fedora_silverblue.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+set -e
+
+if [[ "$EUID" == "0" ]]; then
+   echo "Run this script without sudo!"
+   exit 1
+fi
+
+cd $(dirname $(readlink -f $0))
+
+# adding rpm fusion
+sudo rpm-ostree install \
+    https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm \
+    https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+
+
+sudo yes | sudo rpm-ostree --allow-inactive \
+    mc \
+    vim \
+    git \
+    zsh \
+    htop \
+    ccache \
+    pigz \
+    patool \
+    cmake \
+    neofetch \
+    network-manager-openvpn \
+    openssh-server \
+    gparted \
+    sshfs \
+    gnome-tweaks
+
+. ./install_flatpak.sh
+. ./install_python.sh
+. ./install_oh-my-zsh.sh
+
+echo "###################################"
+echo "Please, log out to apply changes!"
