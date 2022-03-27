214bdb24e1aa80520f385c0676f3dab8d85a7280
Vadym Stupakov <vadim.stupakov@gmail.com>
2022-03-27 18:08:34 +0300

    added force_supersuser flag to use it in docker installations

--- install_linux.py
+++ install_linux.py
@@ -5,10 +5,13 @@ __author__ = "Vadym Stupakov"
 __email__ = "vadim.stupakov@gmail.com"
 
 from subprocess import run
+from shutil import which
 from argparse import ArgumentParser
 import os
 from pathlib import Path
 import sys
+import os
+import stat
 
 ROOT_DIR = Path(__file__).absolute().parent.resolve()
 ENV = os.environ.copy()
@@ -16,6 +19,7 @@ ENV = os.environ.copy()
 if __name__ == '__main__':
     parser = ArgumentParser(prog="install_linux.py")
     parser.add_argument("--mode", help="Installation mode", choices=["gui", "headless"], default="headless")
+    parser.add_argument("--force_superuser", help="Install all from superuser. Use it for docker builds, for example", action="store_true")
 
     args = parser.parse_args()
 
@@ -24,8 +28,20 @@ if __name__ == '__main__':
     ENV.update(args)
     ENV["ROOT_DIR"] = ROOT_DIR.as_posix()
 
+    is_superuser = os.geteuid() == 0
+    cmd = ["/bin/bash", (ROOT_DIR / "lib/install_linux.sh").as_posix()]
+
+    if is_superuser:
+        if args.force_superuser:
+            fake_sudo =  ROOT_DIR / "lib/sudo"
+            fake_sudo.chmod(fake_sudo.stat().st_mode | stat.S_IEXEC)
+            ENV["PATH"] = f"{fake_sudo.parent}{os.pathsep}{ENV['PATH']}"
+        else:
+            print("Run this script without sudo!")
+            sys.exit(-1)
+
     try:
-        ret = run(["/bin/bash", ROOT_DIR / "lib/install_linux.sh"], env=ENV)
+        ret = run(cmd, env=ENV)
         sys.exit(ret.returncode)
     except KeyboardInterrupt:
         sys.exit(0)
