ccbecd927308e0d4d6f01bd10f56bb14a1e52055
Vadym Stupakov <vadim.stupakov@gmail.com>
2022-04-12 11:12:42 +0300

    wip

--- lib/install_python_virtualenv.sh
+++ lib/install_python_virtualenv.sh
@@ -4,21 +4,18 @@ cd $(dirname $(readlink -f $0))
 . header.sh
 
 _VENV_PATH="$HOME/python_envs/default"
-if [ -d "$_VENV_PATH" ]; then
-    rm -rfv "$_VENV_PATH"
-fi
 
-export PYTHONNOUSERSITE=1
 
-python3 -m pip install virtualenv
-python3 -m virtualenv --clear \
-                      --always-copy \
-                      --seeder=pip \
-                      --download \
-                      "$_VENV_PATH"
+/usr/bin/python3 -m pip install virtualenv
+/usr/bin/python3 -m virtualenv  --clear \
+                                --always-copy \
+                                --seeder=pip \
+                                --download \
+                                "$_VENV_PATH"
 
 . "$_VENV_PATH/bin/activate"
-pip install \
+python3 -m pip install \
+    virtualenv \
     pipx \
     ipython \
     jupyterlab \
