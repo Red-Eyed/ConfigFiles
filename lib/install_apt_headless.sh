1cc8477f6c903c5fcab5509024e4fce17705bf66
Vadym Stupakov <vadim.stupakov@gmail.com>
2023-10-28 13:29:15 +0300

    Update install_apt_headless.sh
    added full clang install
    
    https://apt.llvm.org/

--- lib/install_apt_headless.sh
+++ lib/install_apt_headless.sh
@@ -26,9 +26,26 @@ packages="
     cmake
     ninja-build
     g++
-    clang
     clang-format
+    clang-tidy 
+    clang-tools
+    clang
+    clangd
+    libc++-dev
+    libc++1
+    libc++abi-dev
+    libc++abi1
+    libclang-dev
+    libclang1
+    liblldb-dev
+    libllvm-ocaml-dev
+    libomp-dev
+    libomp5 
+    lld
     lldb
+    llvm-dev
+    llvm-runtime
+    llvm python3-clang
     neofetch
     earlyoom
     openssh-server
