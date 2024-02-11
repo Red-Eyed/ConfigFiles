f7155eafb3722220f5a958d32b65627b9bb29ddd
Vadym Stupakov <vadim.stupakov@gmail.com>
2024-02-11 11:48:33 +0200

    Update and rename install_python_virtualenv.sh to install_python_venv.sh
    switched from virtualenv to default venv

---
+++ lib/install_python_venv.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -e
+cd $(dirname $(readlink -f $0))
+. header.sh
+
+_VENV_PATH="$HOME/python_envs/default"
+export PYTHONNOUSERSITE=0
+
+python3 -m venv $_VENV_PATH --copies --clear --upgrade-deps
+. $_VENV_PATH/bin/activate
+
+python3 -m pip install virtualenv pipx
+python3 -m pip install \
+    pipx \
+    ipython \
+    jupyterlab \
+    numpy \
+    pandas \
+    matplotlib \
+    setuptools
