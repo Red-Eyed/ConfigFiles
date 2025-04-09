a2f00172ae9da4ee2517c48ad342ee7698dc0a4e
Vadym Stupakov <vadim.stupakov@gmail.com>
2025-04-09 08:37:34 +0000

    ssh agent wip

--- dotfiles/.config/fish/config.fish
+++ dotfiles/.config/fish/config.fish
@@ -2,9 +2,10 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
-if test -z (pgrep ssh-agent)
-  eval (ssh-agent -c)
-  set -Ux SSH_AUTH_SOCK $SSH_AUTH_SOCK
-  set -Ux SSH_AGENT_PID $SSH_AGENT_PID
-  set -Ux SSH_AUTH_SOCK $SSH_AUTH_SOCK
+if test -z "$SSH_ENV"
+    set -xg SSH_ENV $HOME/.ssh/environment
+end
+
+if not __ssh_agent_is_started
+    __ssh_agent_start
 end
