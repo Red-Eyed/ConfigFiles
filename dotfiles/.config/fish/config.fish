1456e75da514606aaf3e479ae20ed024f860fe2c
Vadym Stupakov <vadim.stupakov@gmail.com>
2025-04-09 09:01:05 +0000

    added rm aliases

--- dotfiles/.config/fish/config.fish
+++ dotfiles/.config/fish/config.fish
@@ -2,7 +2,7 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
-## ls to exa aliases
+## ls aliases
 if type -q exa
     # Use exa as a better ls replacement
     alias ls='exa --group-directories-first --color=always'
@@ -10,6 +10,14 @@ if type -q exa
     alias la='exa -la --group-directories-first --color=always'
     alias lt='exa -lT --level=2 --group-directories-first --color=always'
     alias l.='exa -la --group-directories-first --color=always | grep "^\."'
+end
+
+# rm aliases
+if type -q trashy
+    # Safe replacements for dangerous commands
+    alias rm='trashy'
+    alias rmdir='trashy'
+    alias trash='trashy'
 end
 
 function setup_ssh_agent
