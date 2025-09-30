e95388d15552fa148265f3246c5e3de12ce81553
Vadym Stupakov <vadim.stupakov@gmail.com>
2025-09-30 09:55:40 +0300

    added setting bin dirs in fish config

--- dotfiles/.config/fish/config.fish
+++ dotfiles/.config/fish/config.fish
@@ -20,33 +20,6 @@ if type -q trashy
     alias trash='trashy'
 end
 
-function setup_ssh_agent
-    # Check if keychain is installed and in PATH
-    if not type -q keychain
-        echo "setup_ssh_agent: 'keychain' not found. Please install it to manage your SSH agent."
-        return 1
-    end
 
-    # Call keychain to manage the ssh-agent and output eval commands
-    # `--eval` makes keychain print Bash-style exports
-    # `id_rsa` is the key you want to load (change or add more as needed)
-    for line in (keychain --eval id_rsa)
-
-        # Match only lines that look like: SSH_AUTH_SOCK=...; export SSH_AUTH_SOCK;
-        # We skip anything like 'echo' or unrelated shell statements
-        if string match -rq '^(SSH_.*=)' $line
-
-            # Strip everything after the first semicolon to get: SSH_VAR=value
-            set -l cleaned (string replace -r ';.*' '' -- $line)
-
-            # Split on '=' to get [SSH_VAR, value]
-            set -l kv (string split '=' $cleaned)
-
-            # Export as Fish-style environment variable
-            # Equivalent to: set -gx SSH_AUTH_SOCK /tmp/ssh-xxxxxx/agent.1234
-            set -gx $kv[1] $kv[2]
-        end
-    end
-end
-
-# setup_ssh_agent
+fish_add_path $HOME/.local/bin
+fish_add_path $HOME/.cargo/bin
