4e8d2c4b1318e4039205bcfcfbd47324d4715a38
Vadym Stupakov <vadim.stupakov@gmail.com>
2025-11-09 10:00:09 +0000

    fix(install): Correctly check if oh-my-zsh is already installed
    The conditional check for the existence of the `~/.oh-my-zsh` directory was using incorrect syntax. The negation operator `!` was placed inside the test, causing the check to fail.
    
    This change moves the `!` operator to the correct position outside the `-d` test, ensuring the script only clones the oh-my-zsh repository when it is not already present.

--- lib/install_oh-my-zsh.sh
+++ lib/install_oh-my-zsh.sh
@@ -2,6 +2,6 @@ set -e
 cd $(dirname $(readlink -f $0))
 . header.sh
 
-if [[ -d !~/.oh-my-zsh ]]; then
+if [[ ! -d ~/.oh-my-zsh ]]; then
     git clone https://github.com/ohmyzsh/ohmyzsh.git ~/.oh-my-zsh
 fi
